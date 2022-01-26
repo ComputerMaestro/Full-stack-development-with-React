1c75f95056cf81cda12af6e6296719337fd393ce
yash patel <yp270200@gmail.com>
2022-01-26 16:18:08 +0530

    minified and concatenated css and js files

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Carousel Control
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+
+    // Table Reservation Modal Toggle
+    $('#reserveTableButton').click(function () {
+        $('#reserveTableModal').modal('toggle');
+    });
+
+    // Login Modal Toggle
+    $('#loginButton').click(function () {
+        $('#loginModal').modal('toggle');
+    });
+});
