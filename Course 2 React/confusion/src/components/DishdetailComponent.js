04a4984dd9819b467f2caf8cf540b8354c549b06
yash patel <yp270200@gmail.com>
2022-03-06 17:51:11 +0530

    Add Fetch support

--- Course 2 React/confusion/src/components/DishdetailComponent.js
+++ Course 2 React/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import mapStateToProps from 'react-redux/lib/connect/mapStateToProps';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -119,7 +120,7 @@ function RenderDish({ dish }) {
         if (dish != null) {
             return (<div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
